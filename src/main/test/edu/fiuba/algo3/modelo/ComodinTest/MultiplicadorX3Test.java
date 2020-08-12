93da83326c531e084df5e7bd64f56288ff4eb0c9
Alex <garbieto@fi.uba.ar>
2020-08-12 00:04:13 -0500

    Test comodin terminado

--- src/main/test/edu/fiuba/algo3/modelo/ComodinTest/MultiplicadorX3Test.java
+++ src/main/test/edu/fiuba/algo3/modelo/ComodinTest/MultiplicadorX3Test.java
@@ -1,14 +1,13 @@
 package edu.fiuba.algo3.modelo.ComodinTest;
 
 import edu.fiuba.algo3.modelo.Comodin.Exclusividad;
-import edu.fiuba.algo3.modelo.Comodin.MultiplicadorX2;
+import edu.fiuba.algo3.modelo.Comodin.MultiplicadorX3;
 import edu.fiuba.algo3.modelo.Jugador.Jugador;
 import edu.fiuba.algo3.modelo.Penalidad.Penalidad;
 import edu.fiuba.algo3.modelo.Penalidad.SinPenalidad;
 import edu.fiuba.algo3.modelo.Pregunta.MultiplicadorPreguntaSinPenalidadNoPermitidoException;
 import edu.fiuba.algo3.modelo.Puntaje.Puntaje;
 import edu.fiuba.algo3.modelo.Puntaje.PuntoPositivo;
-import edu.fiuba.algo3.modelo.Puntaje.Puntos;
 import edu.fiuba.algo3.modelo.Respuesta.Respuesta;
 import org.junit.jupiter.api.Test;
 
@@ -16,53 +15,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MultiplicadorX2Test {
+public class MultiplicadorX3Test {
 
     @Test
-    public void test01MultiplicadorX2EsComparadoConOtroMultiplicadorX2DaTrue(){
-        MultiplicadorX2 multiplicadorX2 = new MultiplicadorX2();
+    public void test01MultiplicadorX3EsComparadoConOtroMultiplicadorX3DaTrue(){
+        MultiplicadorX3 multiplicadorX3 = new MultiplicadorX3();
 
-        assertTrue(multiplicadorX2.equals(new MultiplicadorX2()));
+        assertTrue(multiplicadorX3.equals(new MultiplicadorX3()));
     }
 
     @Test
-    public void test02MultiplicadorX2EsComparadoConOtroTipoDeComodinDaFalse(){
-        MultiplicadorX2 multiplicadorX2 = new MultiplicadorX2();
+    public void test02MultiplicadorX3EsComparadoConOtroTipoDeComodinDaFalse(){
+        MultiplicadorX3 multiplicadorX3 = new MultiplicadorX3();
 
-        assertFalse(multiplicadorX2.equals(new Exclusividad()));
+        assertFalse(multiplicadorX3.equals(new Exclusividad()));
     }
 
     @Test
-    public void test03MultiplicadorX2ValidarSinPenalidadLanzaException(){
-        MultiplicadorX2 multiplicadorX2 = new MultiplicadorX2();
+    public void test03MultiplicadorX3ValidarSinPenalidadLanzaException(){
+        MultiplicadorX3 multiplicadorX3 = new MultiplicadorX3();
         Penalidad sinPenalidad = new SinPenalidad();
 
         assertThrows(MultiplicadorPreguntaSinPenalidadNoPermitidoException.class,
                 ()->{
-                    multiplicadorX2.validar(sinPenalidad);
+                    multiplicadorX3.validar(sinPenalidad);
                 });
     }
 
     @Test
-    public void test04MultiplicadorX2SeAplicaAPuntoPositivoYDevuelveElDobleDeEste(){
-        MultiplicadorX2 multiplicadorX2 = new MultiplicadorX2();
+    public void test04MultiplicadorX3SeAplicaAPuntoPositivoYDevuelveElTripleDeEste(){
+        MultiplicadorX3 multiplicadorX3 = new MultiplicadorX3();
         Puntaje puntaje = new PuntoPositivo();
 
-        assertEquals(2, multiplicadorX2.aplicar(puntaje).valorNumerico());
+        assertEquals(3, multiplicadorX3.aplicar(puntaje).valorNumerico());
     }
 
     @Test
-    public void test05MultiplicadorX2SeAplicaADosPuntosYDevuelveElDobleDeEste(){
-        MultiplicadorX2 multiplicadorX2 = new MultiplicadorX2();
+    public void test05MultiplicadorX3SeAplicaADosPuntosYDevuelveElTripleDeEste(){
+        MultiplicadorX3 multiplicadorX3 = new MultiplicadorX3();
         Puntaje puntaje = new PuntoPositivo().agregar(new PuntoPositivo());
 
-        assertEquals(4, multiplicadorX2.aplicar(puntaje).valorNumerico());
+        assertEquals(6, multiplicadorX3.aplicar(puntaje).valorNumerico());
     }
 
     @Test
-    public void test06MultiplicadorX2SeAplicaAListaRespuestasEstasNoSonModificadas(){
-        MultiplicadorX2 multiplicadorX2 = new MultiplicadorX2();
+    public void test06MultiplicadorX3SeAplicaAListaRespuestasEstasNoSonModificadas(){
+        MultiplicadorX3 multiplicadorX3 = new MultiplicadorX3();
         Respuesta respuesta1 = new Respuesta(new Jugador("Carlos"));
         Respuesta respuesta2 = new Respuesta(new Jugador("Luis"));
 
@@ -73,7 +73,7 @@ public class MultiplicadorX2Test {
         respuestas.add(respuesta1);
         respuestas.add(respuesta2);
 
-        multiplicadorX2.aplicar(respuestas);
+        multiplicadorX3.aplicar(respuestas);
 
         assertEquals(1, respuesta1.calificacionValorNumerico());
         assertEquals(1, respuesta2.calificacionValorNumerico());
